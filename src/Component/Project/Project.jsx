2c20b895b1e5df8ee25182c041ca6b983f2b0d17
aman verma <amanv3093@gmai.com>
2024-04-19 13:57:16 +0530

    update project

--- src/Component/Project/Project.jsx
+++ src/Component/Project/Project.jsx
@@ -1,28 +1,42 @@
-import React from 'react'
-import './Project.css'
-import Card from './Card'
-import img1 from '../../Source/Screenshot from 2024-03-17 10-12-39.png'
-import img2 from '../../Source/Screenshot from 2024-03-17 10-31-24.png'
-import img3 from '../../Source/Screenshot from 2024-03-17 10-47-55.png'
-
+import React from "react";
+import "./Project.css";
+import Card from "./Card";
+import img1 from "../../Source/Screenshot from 2024-04-19 13-48-08.png";
+import img2 from "../../Source/Screenshot from 2024-03-17 10-31-24.png";
+import img3 from "../../Source/Screenshot from 2024-04-19 13-48-46.png";
 
 function Project() {
-
-
   return (
-    <div id='project'>
-     <div className='project-heading' data-aos="zoom-out" data-aos-duration="1000">
-       <h2>Project</h2>
-
-     </div>
-     <div className='card-box'>
-      <Card img1={img1} projectName="Github Clone" code="https://github.com/amanv3093/Github-clone" livelink="https://amanv3093.github.io/Github-clone/"/>
-      <Card img1={img2} projectName="Tic-tac-Toi Game" code="https://github.com/amanv3093/Tic-tac-Toi" livelink="https://amanv3093.github.io/Tic-tac-Toi/"/>
-      <Card img1={img3} projectName="NotePad" code="https://github.com/amanv3093/notepad" livelink="https://notepad-bice.vercel.app/"/>
-      
+    <div id="project">
+      <div
+        className="project-heading"
+        data-aos="zoom-out"
+        data-aos-duration="1000"
+      >
+        <h2>Project</h2>
+      </div>
+      <div className="card-box">
+        <Card
+          img1={img1}
+          projectName="TastyTrails"
+          code="https://github.com/amanv3093/TastyTrails"
+          livelink="tasty-trails-phi.vercel.app"
+        />
+        <Card
+          img1={img2}
+          projectName="Tic-tac-Toi Game"
+          code="https://github.com/amanv3093/Tic-tac-Toi"
+          livelink="https://amanv3093.github.io/Tic-tac-Toi/"
+        />
+        <Card
+          img1={img3}
+          projectName="Trendy-Threads"
+          code="https://github.com/amanv3093/Trendy-Threads"
+          livelink="https://trendy-threads.vercel.app/"
+        />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Project
+export default Project;
